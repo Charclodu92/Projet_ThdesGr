c7753fd97c5c2ea1bb4a6bd421732a8c447a647c
Charles CAUSSE <csse.charles@gmail.com>
2025-12-07 16:45:21 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,120 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Démarrage du programme \n");
-
-
-
-        //MENU ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        boolean menu1 = true;
-
-
-        while (menu1 == true){
-            System.out.println("Bienvenue dans le menu !\n" +"Déclinez votre identitée :");
-            System.out.println("1. Collectivité (Mairie, Agglomération)");
-            System.out.println("2. Entreprise de collecte");
-            System.out.println("0. Quitter le programme");
-            System.out.print("Votre choix : ");
-
-            int choixUtilisateur = scanner.nextInt();
-            System.out.println();
-
-            switch (choixUtilisateur) {
+        System.out.println(" \n /////   SYSTÈME DE GESTION DE POUBELLES  /////  ");
+        boolean a = true;
+        while (a) {
+            System.out.println("\nQui êtes-vous ?:");
+            System.out.println("1] Collectivite");
+            System.out.println("2] Entreprise chargé de la collecte ");
+            System.out.println("3] Se deconnecter");
+            System.out.print("Quelle option choisissez-vous ? : ");
+            int choix = scanner.nextInt();
+            switch (choix) {
                 case 1:
-                    //afficherMenuCollectivite(scanner);
+                    menu_collectivite.afficherMenu();
                     break;
                 case 2:
-                    //afficherMenuEntreprise(scanner);
+                    menu_entreprise.afficherMenu();
                     break;
-                case 0:
-                    System.out.println("Fermeture du programme.");
-                    menu1 = false;
+                case 3:
+                    a = false;
                     break;
                 default:
-                    System.out.println("Choix invalide, réessayer.\n");
+                    System.out.println("\n Erreur.\n");
             }
         }
         scanner.close();
     }
 
-
-
-    
-    // sous menu collectivité
-    
-    private static void afficherMenuCollectivite(Scanner scanner) {
-        boolean retour = false;
-        while (!retour) {
-            System.out.println("ESPACE COLLECTIVITÉ ");
-            System.out.println("1. Soumettre un plan / Charger un graphe");
-            System.out.println("2. Signaler des travaux (modifier le graphe)");
-            System.out.println("3. Consulter les statistiques de collecte");
-            System.out.println("0. Retour au menu principal");
-            System.out.print("Votre choix : ");
-
-            int choix = scanner.nextInt();
-            switch (choix) {
-                case 1:
-                    System.out.println("");
-                    // Appeler méthode de création de graphe ici
-                    break;
-                case 2:
-                    System.out.println("");
-                    // Appeler méthode de mise à jour du graphe
-                    break;
-                case 3:
-                    System.out.println("");
-                    break;
-                case 0:
-                    retour = true;
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("Choix est invalide.\n");
-            }
-        }
-    }
-
-
-    
-    // SOUS-MENU ENTREPRISE
-
-    
-    private static void afficherMenuEntreprise(Scanner scanner) {
-        boolean retour = false;
-        while (!retour) {
-            System.out.println("--- ESPACE ENTREPRISE DE COLLECTE ---");
-            System.out.println("1. Calculer une tournée optimale (Voyageur de commerce / MST)");
-            System.out.println("2. Planifier les jours de collecte (Coloration)");
-            System.out.println("3. Gérer la flotte de camions");
-            System.out.println("0. Retour au menu principal");
-            System.out.print("Votre choix : ");
-
-            int choix = scanner.nextInt();
-            switch (choix) {
-                case 1:
-                    System.out.println(">> Lancement de l'algorithme MST / Shortcutting...\n");
-                    // Appeler votre classe Kruskal ou Prim ici
-                    break;
-                case 2:
-                    System.out.println(">> Lancement de l'algorithme de Coloration (Welsh & Powell)...\n");
-                    //  Appeler votre algorithme de coloration ici
-                    break;
-                case 3:
-                    System.out.println(">> Gestion des capacités des camions...\n");
-                    break;
-                case 0:
-                    retour = true;
-                    System.out.println();
-                    break;
-                default:
-                    System.out.println("Choix est invalide.\n");
-            }
-        }
-    }
 }
-
